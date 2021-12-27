017401cdc572fa54307621d54bbf2957f7af7015
holand76 <thegreath76@gmail.com>
2021-12-27 10:53:17 -0300

    usando variavel boolean

--- condicionais/src/Cond.java
+++ condicionais/src/Cond.java
@@ -3,25 +3,25 @@ public class Cond{
 	System.out.println("Testando condicionais");
 	int idade = 15;
 	int quantidade_de_pessoas  = 4;
-	int familiares_maior_de_idade = 1;
+	boolean familiares_maior_de_idade = true;
 	if (idade > 18){
 	System.out.println("Pode passar <_> ");
 	}
 	else{
-		if (quantidade_de_pessoas >1){
-			
-			if (familiares_maior_de_idade > 0){
+		if (quantidade_de_pessoas >1 && familiares_maior_de_idade){
 			System.out.println("Pode passar com os pais ou familiares");
+		}
 		
-			}
-			else{
-			System.out.println("Nao pode entrar !_!");
+		else{
+		  System.out.println("Nao pode entrar !_!");
 			
-			}
 		}
 		
+	
+	}
 		
-		}
 		
 	}
-} 
+		
+}
+ 
