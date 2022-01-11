548cb08c4df208a034b9975373459c047cd2fd97
holand76 <thegreath76@gmail.com>
2022-01-11 13:55:11 -0300

    stuff

---
+++ lacos/Lacos.java
@@ -0,0 +1,28 @@
+public class Lacos{
+	public static void main(String[] args){
+	int contador = 0;
+	while(contador < 10){
+	System.out.println(contador);
+	contador+= 1;
+	
+	
+	}
+	
+	
+	
+	
+	}
+	// exemplificar os 2
+	public static void main2(String[] args){
+	int contador = 0;
+	int i =0;
+	for(i = 0;i<10;i++){
+	System.out.println(i);
+	}
+	
+	
+	
+	
+	}
+
+}
